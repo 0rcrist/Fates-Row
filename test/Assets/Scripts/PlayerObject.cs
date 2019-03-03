4b93136cd432fdeca6c2fb28c848cac6a5bca815
jsb15n <Jsb15n@users.noreply.github.com>
2019-03-03 16:23:09 -0500

    Start of network
    Can spawn player objects from different clients

---
+++ test/Assets/Scripts/PlayerObject.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerObject : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //check if I own this player
+        if (isLocalPlayer == false)
+        {
+            //doesnt belongs to me
+            return;
+        }
+        //PlayerConnection needs to add a visible player
+        //Command the server to spawn the unit
+        CmdSpawnMyUnit();
+    }
+    public GameObject PlayerUnitPrefab;
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Commands are functions that run on the server
+    [Command]
+    void CmdSpawnMyUnit() {
+        GameObject ob = Instantiate(PlayerUnitPrefab);
+        NetworkServer.Spawn(ob);
+    }
+}
