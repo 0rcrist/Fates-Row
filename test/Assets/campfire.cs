e85e309f4374886b66cd219556989395a6bfeda5
Casey <caseykwat@gmail.com>
2019-04-08 13:18:05 -0400

    Created level 1-1, with campfire
    Made a campfire, it kinda sucks. but it works and is animated. Will use the campfire to warp between scenes.

---
+++ test/Assets/campfire.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionSceneChange : MonoBehaviour
+{
+	public string level;
+
+	// Use this for initialization
+	void OnTriggerEnter2D (Collision2D Colider)
+	{
+		if(Colider.gameObject.tag == "Player")
+				Application.LoadLevel(level);
+	}
+}
