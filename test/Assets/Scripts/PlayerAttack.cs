1aa1aa51ff476226a4e464b7076a1b4817e91032
jsb15n <Jsb15n@users.noreply.github.com>
2019-04-17 18:56:47 -0400

    Start on attack
    Created the attack script

---
+++ test/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    [SerializeField]
+    private float timeBtwAttacks;
+    private float startAttacks;
+
+    public Transform attackPos;
+    public float attackRange;
+
+    public LayerMask whatIsEnemy;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void performAttack() {
+        if (timeBtwAttacks <= 0)
+        {
+            timeBtwAttacks = startAttacks;
+            Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemy); 
+        }
+        else
+        {
+            timeBtwAttacks -= Time.fixedDeltaTime;
+        }
+    }
+}
